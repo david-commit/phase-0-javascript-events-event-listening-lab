f4cf91f7c3c4e5ac7aff33df308c32090a984c82
david-commit <david.ondiege@students.moringaschool.com>
2022-08-17 01:19:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+
+
+
+function addingEventListener(){
+    let input = document.getElementById('button');
+    alert('I was clicked!');
+
+    input.addEventListener('click', addingEventListener);
 }
